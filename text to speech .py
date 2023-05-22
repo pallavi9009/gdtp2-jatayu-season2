b0a040b38519e46c5beb862839c00dfec7ba6bb5
pallavi9009 <132778477+pallavi9009@users.noreply.github.com>
2023-05-22 10:53:49 +0530

    Update text to speech .py

--- text to speech .py
+++ text to speech .py
@@ -1,11 +1,13 @@
 import speech_recognition as sr
 import pyttsx3
-listener = sr.Recognizer()
+import tkinter as tk
+from PIL import Image, ImageTk
+
+recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-def talk(text):
-    engine.say(text)
+def text_to_speech():
+    input_text = input_box.get("1.0", tk.END).strip()
+    output_text.delete("1.0", tk.END)
+    output_text.insert(tk.END, input_text)
+    engine.say(input_text)
     engine.runAndWait()
-text = input("Enter the text :")
-talk(text)
